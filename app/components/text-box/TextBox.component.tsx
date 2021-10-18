62290662f30c8a94e8f18b76af98de00cfe62338
Arslanoov <rasularslanoov@gmail.com>
2021-10-18 13:15:44 +0500

    feat(text-box): Added test component

---
+++ app/components/text-box/TextBox.component.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+import styles from './TextBox.module.scss'
+
+type Props = {
+  content: string
+}
+
+const TextBox: React.FC<Props> = ({ content }) => (
+  <div className={styles.textBox}>
+    {content}
+  </div>
+)
+
+export default TextBox
