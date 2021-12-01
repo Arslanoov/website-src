d17c9cacd557faa3631e0c376c567fec235b62d7
Arslanoov <rasularslanoov@gmail.com>
2021-12-01 15:21:54 +0500

    fix(app): Fixed yandex metrika

--- app/src/pages/_app.tsx
+++ app/src/pages/_app.tsx
@@ -2,14 +2,14 @@ import '@/ui/styles/main.scss';
 
 import type { ReactElement, ReactNode } from 'react';
 import { useEffect } from 'react';
-import ym from 'react-yandex-metrika';
 
 import type { NextPage } from 'next';
 import type { AppProps } from 'next/app';
 import Router from 'next/router';
+
 import { SessionProvider } from 'next-auth/react';
 
-import { YMInitializer } from 'react-yandex-metrika';
+import ym, { YMInitializer } from 'react-yandex-metrika';
 
 type NextPageWithLayout = NextPage & {
   getLayout?: (page: ReactElement) => ReactNode
@@ -20,14 +20,14 @@ type AppPropsWithLayout = AppProps & {
 };
 
 Router.events.on('routeChangeComplete', (url: string) => {
-  if (typeof window !== 'undefined' && process.env.NODE_ENV === 'production') {
+  if (typeof window !== 'undefined') {
     ym('hit', url);
   }
 });
 
 const App = ({ Component, pageProps: { session, ...pageProps }}: AppPropsWithLayout) => {
   useEffect(() => {
-    if (typeof window !== 'undefined' && process.env.NODE_ENV === 'production') {
+    if (typeof window !== 'undefined') {
       const url = window.location.pathname + window.location.search;
       ym('hit', url);
     }
@@ -40,16 +40,14 @@ const App = ({ Component, pageProps: { session, ...pageProps }}: AppPropsWithLay
 
   return (
     <>
-      {
-        <YMInitializer
-          accounts={[parseInt(process.env.YM_COUNTER_ID as string)]}
-          options={{
-            webvisor: true,
-            defer: true
-          }}
-          version="2"
-        />
-      }
+      <YMInitializer
+        accounts={[parseInt(process.env.YM_COUNTER_ID as string)]}
+        options={{
+          webvisor: true,
+          defer: true
+        }}
+        version="2"
+      />
       <SessionProvider session={session}>
         {layoutPage}
       </SessionProvider>
