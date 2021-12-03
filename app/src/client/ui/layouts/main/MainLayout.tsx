361918aa017ddb381a432a6ad6cd781838dec90b
Arslanoov <rasularslanoov@gmail.com>
2021-12-03 17:11:43 +0500

    fix(yandex-metrika): Removed dependency

--- app/src/client/ui/layouts/main/MainLayout.tsx
+++ app/src/client/ui/layouts/main/MainLayout.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import Head from 'next/head';
+import Script from 'next/script';
 
 import Header from '@/ui/components/header/Header.component';
 
@@ -23,6 +24,43 @@ const MainLayout: React.FC = ({ children }) => {
           {children}
         </main>
       </div>
+
+      <Script
+        id="yandex-metrika"
+        strategy="afterInteractive"
+        dangerouslySetInnerHTML={{
+          __html: `<script type="text/javascript" >
+            (function (d, w, c) {
+                (w[c] = w[c] || []).push(function() {
+                    try {
+                        w.yaCounter86750287 = new Ya.Metrika({
+                            id:86750287,
+                            clickmap:true,
+                            trackLinks:true,
+                            accurateTrackBounce:true
+                        });
+                    } catch(e) { }
+                });
+        
+                var n = d.getElementsByTagName("script")[0],
+                    s = d.createElement("script"),
+                    f = function () { n.parentNode.insertBefore(s, n); };
+                s.type = "text/javascript";
+                s.async = true;
+                s.src = "https://mc.yandex.ru/metrika/watch.js";
+        
+                if (w.opera == "[object Opera]") {
+                    d.addEventListener("DOMContentLoaded", f, false);
+                } else { f(); }
+            })(document, window, "yandex_metrika_callbacks");
+          </script>
+          <noscript>
+          <div>
+            <img src="https://mc.yandex.ru/watch/86750287" style="position:absolute; left:-9999px;" alt="" />
+          </div>
+          </noscript>`
+        }}
+      />
     </div>
   );
 };
