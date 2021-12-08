df223b8ad0bbbaf07916155caff0cdab0c2e7a4e
Arslanoov <rasularslanoov@gmail.com>
2021-12-08 15:27:30 +0500

    feat(design): Changed content page design

--- app/src/client/ui/components/header/Header.component.tsx
+++ app/src/client/ui/components/header/Header.component.tsx
@@ -9,9 +9,7 @@ const Header = () => {
     <>
       <header className="container">
         <div className={styles.header}>
-          <div>
-            2021
-          </div>
+          <div>2021</div>
           <div className={styles.links}>
             <a className={styles.link} target="_blank" href="mailto:rasul@arslanoov.red" rel="noreferrer">rasul@arslanoov.red</a>
             <a className={styles.link} target="_blank" href="https://github.com/Arslanoov" rel="noreferrer">GitHub</a>
--- app/src/pages/content/[slug].tsx
+++ app/src/pages/content/[slug].tsx
@@ -2,16 +2,15 @@ import type { GetServerSideProps } from 'next';
 import dynamic from 'next/dynamic';
 import { useRouter } from 'next/router';
 
-import ContentMoreButton from '@/ui/components/content-list/more-button/ContentMoreButton.component';
-
 import { ContentItem as ContentItemInterface } from '@/domain/content/contentItem';
+
+import { dateFormatter } from '@/app/utils/date/formatter';
 
 import getOneContentItemCommand from '@/api/useCases/contentItem/getOne/command';
 import getOneContentItemHandler from '@/api/useCases/contentItem/getOne/handler';
 
-import { dateFormatter } from '@/app/utils/date/formatter';
-
 import MainLayout from '@/ui/layouts/main/MainLayout';
+import ContentMoreButton from '@/ui/components/content-list/more-button/ContentMoreButton.component';
 
 import styles from '@/ui/styles/pages/content-item.module.scss';
 
@@ -37,31 +36,33 @@ export default function ContentItem({ contentItem }: Props) {
   const { locale } = useRouter();
 
   return (
-    <>
+    <div className={styles.page}>
       <div className="container">
         <div className={styles.wrapper}>
-          <div className={styles.button}>
-            <ContentMoreButton text="Go back" goBack />
-          </div>
-          <h1 className={styles.title}>
-            {contentItem.title}
-          </h1>
-          <div className={styles.date}>
-            {dateFormatter(contentItem.createdAt, locale)}
+          {contentItem.cover && <img
+            src={contentItem.cover}
+            className={styles.image}
+            draggable={false}
+            alt=""
+          />}
+
+          <div className={styles.content}>
+            <div className={styles.row}>
+              <h1 className={styles.title}>
+                {contentItem.title}
+              </h1>
+              <ContentMoreButton text="Back" goBack />
+            </div>
+            <div className={styles.date}>
+              {dateFormatter(contentItem.createdAt, locale)}
+            </div>
+            <p className={styles.description}>{contentItem.description}</p>
           </div>
         </div>
       </div>
 
-      {contentItem.cover && <img
-        src={contentItem.cover}
-        className={styles.image}
-        draggable={false}
-        alt=""
-      />}
-
       <div className="container">
         <div className={styles.wrapper}>
-          <p className={styles.description}>{contentItem.description}</p>
           <div className={styles.content}>
             <Editor
               initialValue={JSON.parse(contentItem.rawContent)}
@@ -71,7 +72,7 @@ export default function ContentItem({ contentItem }: Props) {
           </div>
         </div>
       </div>
-    </>
+    </div>
   );
 };
 
