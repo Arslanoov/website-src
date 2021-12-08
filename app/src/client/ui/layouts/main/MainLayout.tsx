b2d7b0168e8a55bb67a2c27bf629163e4cd34a88
Arslanoov <rasularslanoov@gmail.com>
2021-12-08 15:33:47 +0500

    feat(design): Changed favico

--- app/src/client/ui/layouts/main/MainLayout.tsx
+++ app/src/client/ui/layouts/main/MainLayout.tsx
@@ -14,7 +14,10 @@ const MainLayout: React.FC = ({ children }) => {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <meta name="description" content="Red" />
         <meta name="yandex-verification" content="c6e27706431d2eba" />
-        <link rel="icon" href="/favicon.ico" />
+        <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
+        <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
+        <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
+        <link rel="manifest" href="/site.webmanifest" />
       </Head>
 
       <div className={styles.layout}>
