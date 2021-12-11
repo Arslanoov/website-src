43d586fc15afda9018ab54be621f13ffd30397af
Arslanoov <rasularslanoov@gmail.com>
2021-12-11 14:58:13 +0500

    feat(robots): Added robots.txt

--- app/src/client/ui/layouts/main/MainLayout.tsx
+++ app/src/client/ui/layouts/main/MainLayout.tsx
@@ -19,7 +19,7 @@ const MainLayout: React.FC<Props> = ({
       <Head>
         <title>{title ? `Arslanoov - ${title}` : 'Arslanoov'}</title>
         <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <meta name="description" content="Red" />
+        <meta name="description" content="Frontend developer website." />
         <meta name="yandex-verification" content="c6e27706431d2eba" />
         <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
         <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
