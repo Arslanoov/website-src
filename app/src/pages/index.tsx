5fd55f3eb8fcad4216bc47224bc5daa934e2872e
Arslanoov <rasularslanoov@gmail.com>
2022-03-09 22:18:09 +0500

    fix(manage): Fixed manage button styles

--- app/src/pages/index.tsx
+++ app/src/pages/index.tsx
@@ -1,11 +1,10 @@
 import React from 'react';
+import Router from 'next/router';
 
 import { useSession } from 'next-auth/react';
-import { useRouter } from 'next/router';
 
 import { AuthStatus, SessionUserInterface } from '@/domain/user/auth';
 import { UserRole } from '@/domain/user/user';
-import { LanguageType } from '@/domain/content/contentItem';
 
 import MainLayout from '@/ui/layouts/main/MainLayout';
 import AboutMe from '@/ui/sections/about-me/AboutMe';
@@ -18,17 +17,17 @@ export default function Home() {
   const { status, data: session } = useSession();
   const user = session?.user as SessionUserInterface | null;
 
+  const toManagePage = () => Router.push('/manage/content/list');
+
   return (
     <>
       <AboutMe />
       {
         status === AuthStatus.logged &&
         user.role === UserRole.Admin &&
-        <div className="container">
-          <div className={styles.button}>
-            <ContentMoreButton text="Manage" link="/manage/content/list" />
-          </div>
-        </div>
+        <button onClick={toManagePage} className={styles.button}>
+          Manage
+        </button>
       }
     </>
   );
